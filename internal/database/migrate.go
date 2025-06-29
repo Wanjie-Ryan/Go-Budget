c82d92532321f86ccb582b943b882c1debecafad
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-06-29 16:18:49 +0300

    parsing and binding

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,2 +1,27 @@
-package database
+package main
 
+import (
+	"fmt"
+
+	"github.com/Wanjie-Ryan/Go-Budget/common"
+	"github.com/Wanjie-Ryan/Go-Budget/internal/models"
+)
+
+func main() {
+
+	db, err := common.NewMySql()
+	if err != nil {
+		// panic stops normal execution and unwinds the stack, prints the error and stack trace to the console
+		panic(err)
+	}
+	err = db.AutoMigrate(&models.UserModel{})
+	// err = db.Migrator().AlterColumn(&models.UserModel{}, "Firstname")
+	// err = db.Migrator().AlterColumn(&models.UserModel{}, "Lastname")
+	// err = db.Migrator().AlterColumn(&models.UserModel{}, "Email")
+	// err = db.Migrator().AlterColumn(&models.UserModel{}, "Gender")
+	// err = db.Migrator().AlterColumn(&models.UserModel{}, "password")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database Migrated")
+}
--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -6,9 +6,14 @@ type UserModel struct {
 	// if you see fields may become null when entering data, assign them as pointers  using the *
 	// the gorm.Model in it carries the id, updated at, created at and deleted at
 	gorm.Model
-	Firstname *string
-	Lastname  *string
-	Email     string
-	Gender    *string
-	Password  string
+	Firstname *string `gorm:"type:varchar(200)"`
+	Lastname  *string `gorm:"type:varchar(200)"`
+	Email     string  `gorm:"type:varchar(100); not null; unique"`
+	Gender    *string `gorm:"type:varchar(50)"`
+	Password  string  `gorm:"type:varchar(200); not null"`
 }
+
+// if you want to change the table name, to a custom name, the function below helps, instead of the table being named UserModel, it will be named users
+// func (receiver UserModel) TableName() string{
+// 	return "users"
+// }
