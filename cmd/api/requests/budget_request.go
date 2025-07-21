adcfa14db23e527c4f2365fa378dc53927fe9c6e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-07-21 22:05:29 +0300

    budgetmodel and migrations && creation

---
+++ cmd/api/requests/budget_request.go
@@ -0,0 +1,24 @@
+package request
+
+// request to store and update a budget
+type CreateBudgetRequest struct {
+
+	// the dive attribute goes through the array (each of the item) in categories and ensures that all of them are of the type uint64
+	// uint --> unsigned integer, cannot be NEGATIVE
+	Categories []uint64 `json:"categories" validate:"required, dive, min=1"`
+	Amount     float64  `json:"amount" validate:"required, numeric, min=1"`
+	//omitempty --> its ok if the user doesn't provide this
+	// the format date is type YYYY-MM-DD
+	Date  string `json:"date,omitempty" validate:"omitempty, datetime=2006-01-02"`
+	Title string `json:"title" validate:"required, min=2, max=200"`
+}
+
+type UpdateBudgetRequest struct {
+	Categories []uint64 `json:"categories" validate:"omitempty, dive, min=1"`
+	Amount     float64  `json:"amount" validate:"omitempty, numeric, min=1"`
+	Date       string   `json:"date,omitempty" validate:"omitempty, datetime=2006-01-02"`
+	Title      string   `json:"title" validate:"omitempty, min=2, max=200"`
+}
+
+// the omitempty being in the json tag means if the field is empty, don't include it in the json output
+//in the validate field, the omitempty means that if the field is empty, don't validate
